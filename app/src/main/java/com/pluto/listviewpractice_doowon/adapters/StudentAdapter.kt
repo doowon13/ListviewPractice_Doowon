ac872ec111e7d3e93da40778dedd2ee6c629c1f2
doowon13 <endnjs2007@gmail.com>
2021-05-19 17:26:45 +0900

    Adapter 뼈대작업 마무리

--- app/src/main/java/com/pluto/listviewpractice_doowon/adapters/StudentAdapter.kt
+++ app/src/main/java/com/pluto/listviewpractice_doowon/adapters/StudentAdapter.kt
@@ -1,11 +1,29 @@
 package com.pluto.listviewpractice_doowon.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.pluto.listviewpractice_doowon.R
 import com.pluto.listviewpractice_doowon.datas.Students
 
 class StudentAdapter(
     val mContext : Context,
     val resID : Int,
     val mList : ArrayList<Students>) : ArrayAdapter<Students>(mContext, resID, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 }
