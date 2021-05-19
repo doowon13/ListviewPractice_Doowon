8b90886834536b767f2a1623c68a42de6a4b2352
doowon13 <endnjs2007@gmail.com>
2021-05-19 17:35:35 +0900

    학생 목록 ArrayList를 생성하고 실제 목록을 추가해주었다.

--- app/src/main/java/com/pluto/listviewpractice_doowon/MainActivity.kt
+++ app/src/main/java/com/pluto/listviewpractice_doowon/MainActivity.kt
@@ -2,10 +2,21 @@ package com.pluto.listviewpractice_doowon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pluto.listviewpractice_doowon.datas.Students
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Students>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Students("허두원", 1994))
+        mStudentList.add(Students("김철수", 1992))
+        mStudentList.add(Students("박영희", 1996))
+        mStudentList.add(Students("이기영", 1999))
+        mStudentList.add(Students("신수진", 1998))
+        mStudentList.add(Students("장영수", 1991))
     }
 }
