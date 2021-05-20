5d9b5477482818747eff97b556ae3c57173d6506
doowon13 <endnjs2007@gmail.com>
2021-05-20 12:14:02 +0900

    리스트뷰의 아이템 길게 누르는 이벤트 처리

--- app/src/main/java/com/pluto/listviewpractice_doowon/MainActivity.kt
+++ app/src/main/java/com/pluto/listviewpractice_doowon/MainActivity.kt
@@ -35,5 +35,14 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "${clickedStudent.name}이 클릭 됨", Toast.LENGTH_SHORT).show()
 
         }
+
+        studentListView.setOnItemLongClickListener { parent, view, position, id ->
+
+            val longClickedStudent = mStudentList[position]
+
+            Toast.makeText(this, "${longClickedStudent.name}이 길게 클릭 됨", Toast.LENGTH_SHORT).show()
+
+            return@setOnItemLongClickListener true
+        }
     }
 }
